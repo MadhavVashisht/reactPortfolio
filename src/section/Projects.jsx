90f91d7dbf4395f4ef4093b01fecd02a2b8619a0
Madhav Vashisht <vashishthmadhav1906@gmail.com>
2025-12-17 02:44:04 +0530

    bugfixes

--- src/section/Projects.jsx
+++ src/section/Projects.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useMemo, useRef, useState } from "react";
-import img1 from "../assets/img1.jpg";
-import img2 from "../assets/img2.jpg";
-import img3 from "../assets/img3.jpg";
-import photo1 from "../assets/photo1.jpg";
+import img1 from "../assets/img1.JPG";
+import img2 from "../assets/img2.JPG";
+import img3 from "../assets/img3.JPG";
+import photo1 from "../assets/photo1.JPG";
 import photo2 from "../assets/photo2.PNG";
 import photo3 from "../assets/photo3.png";
 import {
